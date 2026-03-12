1e372a4405320585241f7bf6d408bdc88c770239
Ayush8481Lab <wonder945177@gmail.com>
2026-03-12 23:26:23 +0530

    Update video.js

--- api/video.js
+++ api/video.js
@@ -1,31 +1,12 @@
-import crypto from "crypto";
-
 export default function handler(req,res){
 
 const ref = req.headers.referer || "";
 const origin = req.headers.origin || "";
 
+/* allow only your domain */
+
 if(!ref.includes("ayushyad.vercel.app") && !origin.includes("ayushyad.vercel.app")){
 return res.status(403).send("Forbidden");
-}
-
-const {token,expire}=req.query;
-
-if(!token || !expire){
-return res.status(403).send("Invalid");
-}
-
-if(expire < Math.floor(Date.now()/1000)){
-return res.status(403).send("Expired");
-}
-
-const valid = crypto
-.createHmac("sha256",process.env.SECRET_KEY)
-.update(String(expire))
-.digest("hex");
-
-if(token !== valid){
-return res.status(403).send("Bad token");
 }
 
 const drive = `https://drive.google.com/file/d/${process.env.DRIVE_ID}/preview`;
@@ -33,13 +14,23 @@ const drive = `https://drive.google.com/file/d/${process.env.DRIVE_ID}/preview`;
 res.send(`
 <!DOCTYPE html>
 <html>
-<body style="margin:0;background:black">
+<head>
+<style>
+body{
+margin:0;
+background:black;
+}
+iframe{
+border:none;
+width:100%;
+height:100vh;
+}
+</style>
+</head>
 
-<iframe
-src="${drive}"
-style="border:none;width:100%;height:100vh"
-allow="autoplay;fullscreen">
-</iframe>
+<body>
+
+<iframe src="${drive}" allow="autoplay;fullscreen"></iframe>
 
 </body>
 </html>
