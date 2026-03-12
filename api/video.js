7567311bf88668afcd56f23af5a247a13ce233f3
Ayush8481Lab <wonder945177@gmail.com>
2026-03-12 22:31:37 +0530

    Update video.js

--- api/video.js
+++ api/video.js
@@ -3,14 +3,15 @@ import crypto from "crypto";
 export default function handler(req,res){
 
 const ref = req.headers.referer || "";
+const origin = req.headers.origin || "";
 
-if(!ref.includes("ayushyad.vercel.app")){
+if(!ref.includes("ayushyad.vercel.app") && !origin.includes("ayushyad.vercel.app")){
 return res.status(403).send("Forbidden");
 }
 
 const {token,expire}=req.query;
 
-if(!token||!expire){
+if(!token || !expire){
 return res.status(403).send("Invalid");
 }
 
@@ -23,12 +24,25 @@ const valid = crypto
 .update(String(expire))
 .digest("hex");
 
-if(token!==valid){
+if(token !== valid){
 return res.status(403).send("Bad token");
 }
 
-res.json({
-embed:`https://drive.google.com/file/d/${process.env.DRIVE_ID}/preview`
-});
+const drive = `https://drive.google.com/file/d/${process.env.DRIVE_ID}/preview`;
+
+res.send(`
+<!DOCTYPE html>
+<html>
+<body style="margin:0;background:black">
+
+<iframe
+src="${drive}"
+style="border:none;width:100%;height:100vh"
+allow="autoplay;fullscreen">
+</iframe>
+
+</body>
+</html>
+`);
 
 }
