bd1726dc205d4649e0701762a469d4d3aee30e8d
Ayush8481Lab <wonder945177@gmail.com>
2026-03-12 22:17:17 +0530

    Update video.js

--- api/video.js
+++ api/video.js
@@ -1,34 +1,34 @@
 import crypto from "crypto";
 
-export default function handler(req, res) {
+export default function handler(req,res){
 
 const ref = req.headers.referer || "";
 
-if (!ref.includes(process.env.SITE)) {
-return res.status(403).send("Blocked");
+if(!ref.includes("ayushyad.vercel.app")){
+return res.status(403).send("Forbidden");
 }
 
-const { token, expire } = req.query;
+const {token,expire}=req.query;
 
-if (!token || !expire) {
+if(!token||!expire){
 return res.status(403).send("Invalid");
 }
 
-if (expire < Math.floor(Date.now()/1000)) {
+if(expire < Math.floor(Date.now()/1000)){
 return res.status(403).send("Expired");
 }
 
 const valid = crypto
-.createHmac("sha256", process.env.SECRET_KEY)
+.createHmac("sha256",process.env.SECRET_KEY)
 .update(String(expire))
 .digest("hex");
 
-if (token !== valid) {
+if(token!==valid){
 return res.status(403).send("Bad token");
 }
 
-const url = `https://drive.google.com/file/d/${process.env.DRIVE_ID}/preview`;
-
-res.redirect(url);
+res.json({
+embed:`https://drive.google.com/file/d/${process.env.DRIVE_ID}/preview`
+});
 
 }
