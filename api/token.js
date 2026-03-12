8bc1cec0ae43e1c96abdb2e824ba5200b1a8817a
Ayush8481Lab <wonder945177@gmail.com>
2026-03-12 22:07:22 +0530

    Update token.js

--- api/token.js
+++ api/token.js
@@ -2,18 +2,19 @@ import crypto from "crypto";
 
 export default function handler(req, res) {
 
-const secret = process.env.SECRET_KEY;
+const ref = req.headers.referer || "";
+
+if (!ref.includes(process.env.SITE)) {
+return res.status(403).send("Forbidden");
+}
 
 const expire = Math.floor(Date.now()/1000) + 600;
 
 const token = crypto
-.createHmac("sha256", secret)
+.createHmac("sha256", process.env.SECRET_KEY)
 .update(String(expire))
 .digest("hex");
 
-res.status(200).json({
-token,
-expire
-});
+res.json({ token, expire });
 
 }
